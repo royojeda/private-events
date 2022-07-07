cd960a2109b1416d09f4e3cd3af5b391d2bbb214
Dennis Ojeda <royojeda1@gmail.com>
2022-07-07 15:32:38 +0800

    Add creator-created_event association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :created_events, class_name: "Event",
+                            foreign_key: "creator_id",
+                            inverse_of: "creator"
 end
