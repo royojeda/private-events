f1f9ff834586c4b641ec26781f9da3ae0c092d36
Dennis Ojeda <royojeda1@gmail.com>
2022-07-08 23:39:05 +0800

    Update events#index to separate past from future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
+  def self.past
+    where("date < ?", Date.current)
+  end
+
+  def self.future
+    where("date >= ?", Date.current)
+  end
+
   belongs_to :creator, class_name: "User"
 
   has_many :attendances, foreign_key: :attended_event_id,
