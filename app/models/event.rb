dff6a3865a3038890db80c2b91052f5f751ac1ee
Dennis Ojeda <royojeda1@gmail.com>
2022-07-08 15:24:01 +0800

    Add attendee-attendance-attended_event association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+
+  has_many :attendances, foreign_key: :attended_event_id,
+                         inverse_of: :attended_event
+  has_many :attendees, through: :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :created_events, class_name: "Event",
                             foreign_key: "creator_id",
                             inverse_of: "creator"
+
+  has_many :attendances, foreign_key: :attendee_id,
+                         inverse_of: :attendee
+  has_many :attended_events, through: :attendances
 end
