c660ac2fd54aa0d3c693c746de08ceaa6dbb6452
Dennis Ojeda <royojeda1@gmail.com>
2022-07-09 16:23:48 +0800

    Fix bug with CreateAttendances migration

--- db/migrate/20220708071042_create_attendances.rb
+++ db/migrate/20220708071042_create_attendances.rb
@@ -1,6 +1,6 @@
 class CreateAttendances < ActiveRecord::Migration[7.0]
   def change
-    drop_table :attendances
+    drop_table :attendances, if_exists: true
     create_table :attendances do |t|
       t.belongs_to :attendee
       t.belongs_to :attended_event
